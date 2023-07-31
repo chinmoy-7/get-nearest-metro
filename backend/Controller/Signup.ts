f50f5883b7ead7b1caf024f68cbfb2f8b0aec6ec
Chinmoy Dehingia <chinmoy.dehingia@gmail.com>
2023-07-31 23:07:16 +0530

    bug fix

--- backend/Controller/Signup.ts
+++ backend/Controller/Signup.ts
@@ -12,7 +12,7 @@ export const getMetro = async (
     const { lat, long } = req.body;
     const ipAddress:any =req.headers["x-forwarded-for"] || req.socket.remoteAddress;
     const result=await sendAPIRequest(JSON.stringify(ipAddress))
-
+    console.log(process.env.X_RAPID_KEY,"<enviroment=================",result )
     if(result.region_iso_code=="DL"){
     const options = {
       method: "GET",
